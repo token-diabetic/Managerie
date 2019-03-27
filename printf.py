c48012a6478a2f46affc836e4d8e24b6061399e2
token-diabetic <token-diabetic@users.noreply.github.com>
2019-03-26 21:34:17 -0400

    attempt 3

--- printf.py
+++ printf.py
@@ -4,13 +4,11 @@ Prints text in a fancy way, like in the movies.
 
 import time, sys
 
-def printf(s, speed):
-    try:
-        speed = int(speed)
-    except (TypeError, ValueError):
-        speed = 0.005
+def printf(s, speed=10):
+    if not isinstance(speed, int) or isinstance(speed, float):
+        speed = 10
     for c in s:
         sys.stdout.write(c)
         sys.stdout.flush()
-        time.sleep(speed)
+        time.sleep(speed / 1000)
     sys.stdout.write("\n")
---
+++ printftest.py
@@ -0,0 +1,16 @@
+"""
+Use for testing printf.py
+"""
+
+from printf import printf
+
+printf("THIS IS A TEST OF THE PRINTF WITH NO SPEED ARGUMENT GIVEN TO INPUT")
+printf("THIS IS A TEST OF THE PRINTF WITH A SPEED ARGUMENT OF 1ms", 1)
+printf("THIS IS A TEST OF THE PRINTF WITH A SPEED ARGUMENT OF 100ms", 100)
+printf('''
+Eu non duis amet aliquip minim ad ad cillum commodo pariatur deserunt voluptate ad.
+Exercitation id adipisicing pariatur commodo eiusmod officia magna qui duis anim non exercitation aliqua.
+Sunt ipsum veniam tempor sit ex adipisicing ipsum proident ut minim irure velit adipisicing mollit.
+Cillum nulla et aliquip minim commodo ad cupidatat enim amet eiusmod laborum magna voluptate culpa.
+MULTI-LINE TEST COMPLETE.
+''')
